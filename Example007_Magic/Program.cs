b0b6b89c9c74c61038d60b5e6a3821450401ad3c
Scherbakov Anatoly <axidarr@gmail.com>
2022-08-18 15:00:39 +0300

    Add some fixes

--- Example007_Magic/Program.cs
+++ Example007_Magic/Program.cs
@@ -19,20 +19,20 @@ int x = xa, y = ya;
 
 int count = 0;
 
-while(count < 5000)
+while (count < 5000)
 {
     int what = new Random().Next(0, 3); // 0 1 2
-    if(what == 0)
+    if (what == 0)
     {
         x = (x + xa) / 2;
         y = (y + ya) / 2;
     }
-    if(what == 1)
+    if (what == 1)
     {
         x = (x + xb) / 2;
         y = (y + yb) / 2;
     }
-    if(what == 2)
+    if (what == 2)
     {
         x = (x + xc) / 2;
         y = (y + yc) / 2;
@@ -42,3 +42,4 @@ while(count < 5000)
     Console.WriteLine("+");
     count++;
 }
+Console.ReadLine();
---
+++ Seminar1_001_Dog_and_friends(Nataly)/Program.cs
@@ -0,0 +1,23 @@
+﻿int firstSpeed = 4, secondSpeed = 5, dogspeed = 10;
+int friend = 2;
+int count = 0, distance = 1000, time = 0;
+
+while(distance > 10)
+{
+    if(friend == 1)
+    {
+       time = distance / (firstSpeed + dogspeed);
+       friend = 2;
+
+    }
+    if(friend == 2)
+    {
+        time = distance / (secondSpeed + dogspeed);
+        friend = 1;
+    }
+distance = distance - time * (firstSpeed + secondSpeed);
+count += 1;
+}
+Console.Write("Собака пробeжит ");
+Console.Write(count);
+Console.Write("раз");

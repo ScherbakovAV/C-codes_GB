9ebf4374446e22d3cc945869307928030f155b0a
Scherbakov Anatoly <axidarr@gmail.com>
2022-08-21 01:16:30 +0300

    Add new examples from Lesson2 (008-011)

---
+++ Lesson2/Example010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 51, 48, 96, 62, 87, 1, 96, 78 };
+
+int n = array.Length;
+int find = 96;
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; // чтобы поиск останавливался при первом же результате, если есть одинаковые элементы массива 
+    }
+    index++;
+}
---
+++ Lesson2/Example011_ArrayLibrary/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задаём рандомный массив из n элементов, в котором необходимо найти заданное число
+
+void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while (index < lenght)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1; // вместо "0", чтобы в случае ненахождения искомого числа на экран выводилось "-1"
+
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[10]; // массив из 10 элементов, по умолчанию заполненный нулями
+// чтобы изменить умолчание, используем метод (вверху)
+
+FillArray(array); // заполнение массива
+PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 9);
+Console.WriteLine(pos);

25f9187bb8e2deb6772ea2b10418215a65f7c8ab
Scherbakov Anatoly <axidarr@gmail.com>
2022-08-23 13:18:10 +0300

    Added ex. of Seminar 2 from Roman Korobkov and changed my ex. of it Seminar

--- Seminar2/Seminar2_001/Program.cs
+++ Seminar2/Seminar2_001/Program.cs
@@ -1,49 +1,27 @@
-﻿// написать программу, которая выдаёт случайное число из отрезка [10, 99]
-// и показывает наибольшую цифру числа
+﻿// Написать программу, которая выдаёт случайное число из отрезка и показывает наибольшую цифру числа.
 
-using System;
+// Решение преподавателя в моей переработке.
 
-namespace Seminar2
+static int MaxNumber() // статический метод
 {
-    class Program
+    int number1 = new Random().Next(10, 1000000);
+    int max = 0;
+    int iterator = number1;
+
+    Console.WriteLine("Случайное число от 10 до 999999 - "+number1);
+
+    while(iterator != 0)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        int number2 = iterator % 10;
 
-        static int MaxNumber() // это статический метод
-        {
-            int number1 = new Random().Next(10, 1000);
-            int max = 0;
-            int iterator = number1;
+        if (number2 > max) max = number2;
 
-            while(iterator != 0)
-            {
-                int number2 = iterator % 10;
-
-                if (number2 > max) max = number2;
-
-                iterator /= 10;
-                // iterator = iterator / 10
-            }
-
-        }
-
-        static int MaxNumber2() // это статический метод
-        {
-            int number = new Random().Next(10, 1000);
-            int number1 = number % 10;
-            int number2 = number % 10;
-
-            int max = number1;
-
-            if (number1 < number2)
-            {
-                max = number2;
-            }
-            System.Console.WriteLine("null");
-
-        }
+        iterator /= 10;
+        // iterator = iterator / 10
     }
+
+    return max;
+
 }
+
+    Console.WriteLine("Максимальная цифра этого числа - "+MaxNumber());

e11fb00657a53f2f43fbefdb2902ad4459aea174
ScherbakovA <109811014+ScherbakovAV@users.noreply.github.com>
2022-09-09 11:11:00 +0300

    Update program.cs in Seminar 6 (from teacher)

--- Seminar6/Seminar6_from teacher/Program.cs
+++ Seminar6/Seminar6_from teacher/Program.cs
@@ -1,18 +1,24 @@
-﻿//Напишите программу, которая принимает на вход три числа и проверяет, 
-//может ли существовать треугольник с сторонами такой длины.
+using System;
 
-Console.Clear();
-Console.Write("Введите стороны треугольника через пробел: ");
+class Program {
+  public static void Main (string[] args) {
+
+    Console.Clear();
+//Напишите программу, которая принимает на вход три числа и проверяет, 
+//может ли существовать треугольник с сторонами такой длины.
+Console.WriteLine($"---Проверка возможности существования треугольника с заданными сторонами---\n");
+
+Console.Write("Введите стороны треугольника через пробел:\t");
 
 string[] st = Console.ReadLine()!.Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
 if (IsTriangle(int.Parse(st[0]), int.Parse(st[1]), int.Parse(st[2])))
 {
-    Console.WriteLine("Да");
+    Console.WriteLine("\nТакой треугольник существует");
 }
 else
 {
-    Console.WriteLine("Нет");
+    Console.WriteLine("\nТакой треугольник не может существовать");
 }
 
 bool IsTriangle(int a, int b, int c)
@@ -24,33 +30,26 @@ bool IsTriangle(int a, int b, int c)
 //Напишите программу, 
 //которая будет преобразовывать десятичное число в двоичное.
 
-Console.WriteLine("Введите десятичное число: ");
-int number = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"\n---Преобразование числа из двоичной системы счисления в двоичную---\n");
+    
+    Console.Write("Введите десятичное число:\t");
+        
+    int decNum = Convert.ToInt32(Console.ReadLine());
 
-string res1 = Convert.ToString(number, 2);
-string res2 = DecToNum(number, 2);
+    string res1 = Convert.ToString(decNum, 2);
+    Console.WriteLine($"\nПо методу 1: {decNum} -> {res1}\n");
 
-Console.WriteLine($"{number}->{res1}");
-Console.WriteLine($"{number}->{res2}");
-
-
-//Универсальный математический для перевода из 10 в любую
-string DecToNum(int decNumber, int otherSystem)
-{
-    string res = "";
-    string nums = "0123456789ABCDEF";
-    while (decNumber > 0)
-    {
-        int ost = decNumber / otherSystem;
-        res = nums[decNumber - otherSystem * ost] + res;
-        decNumber /= otherSystem;
-    }
-    return res;
-}
+    string res2 = String.Join("", DecToNumBool(decNum));
+    Console.Write($"По методу 2: {decNum} -> ");
+    Console.WriteLine(res2.Replace("True", "1").Replace("False", "0"));
+    
+    string res3 = DecToNumUniversal(decNum, 2);
+    Console.WriteLine($"\nПо методу 3: {decNum} -> {res3}\n");
+    
 //Универсальный математический для перевода из 10 в двоичную
-bool[] DecToNum2(int num)
+    
+bool[] DecToNumBool(int num) // для метода 2
 {
-
     int result = 0;
     bool[] arr = new bool[32];
     int i;
@@ -62,17 +61,32 @@ bool[] DecToNum2(int num)
     }
     return arr;
 }
+  
+//Универсальный математический для перевода из 10 в любую
+string DecToNumUniversal(int decNumber, int otherSystem = 2) // для метода 3
+{
+    string res = String.Empty;
+    string nums = "0123456789ABCDEF";
+    int ost;
+      
+    while (decNumber > 0)
+    {
+        ost = decNumber / otherSystem;
+        res = nums[decNumber - otherSystem * ost] + res;
+        decNumber = ost;
+    }
+    return res;
+}
 
-Console.WriteLine("Введите десятичное число:");
-int decNum = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Это число в двоичной системе счисления: {DecToNum2(decNum)}");
 // Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
 
-int[] arr = GetArray();
-Console.WriteLine(String.Join(" ", arr));
+    Console.WriteLine($"\n---Создание копии заданного массива с помощью поэлементного копирования---\n");
+    
+int[] array = GetArray();
+Console.WriteLine($"\nЗаданный рандомный массив:\n[{String.Join(", ", array)}]\n");
 
-int[] copyArray = CopyArray(arr);
-Console.WriteLine(String.Join(" ", copyArray));
+int[] copyArray = CopyArray(array);
+Console.WriteLine($"Копия этого массива:\n[{String.Join(", ", copyArray)}]\n");
 
 int[] CopyArray(int[] inArray)
 {
@@ -85,14 +99,73 @@ int[] CopyArray(int[] inArray)
     return result;
 }
 
-//Задача 44: Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
-double FibonacciBad(int n)
+    int[] GetArray()
 {
-    if (n == 1 || n == 2) return 1;
+    Console.Write("Введите размер массива:\t");
+    int size = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите минимальное число массива:\t");
+    int minValue = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Введите максимальное число массива:\t");
+    int maxValue = Convert.ToInt32(Console.ReadLine());
+
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+
+//Напишите программу, которая перевернёт одномерный массив 
+//(последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+Console.WriteLine($"\nПеревёрнутый массив по методу 1:\n[{String.Join(", ", ReversArray1(array))}]\n");
+    
+Console.WriteLine($"\nПеревёрнутый массив по методу 2:\n[{String.Join(", ", ReversArray2(copyArray))}]\n");
+
+Array.Reverse(copyArray);
+    
+Console.WriteLine($"\nПеревёрнутый массив по методу 3:\n[{String.Join(", ", copyArray)}]\n");
+
+
+int[] ReversArray1(int[] inArray) // этот метод изменяет входящий массив, насколько я понял
+{
+    int[] outArray = new int[inArray.Length];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        int numTemp = inArray[i];
+        outArray[i] = inArray[inArray.Length - i - 1];
+        inArray[inArray.Length - i - 1] = numTemp;
+    }
+    return outArray;
+}
+
+int[] ReversArray2(int[] inArray) // этот метод оставляет входящий массив без изменений
+{
+    int[] result = new int[inArray.Length];
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        result[i] = inArray[inArray.Length - 1 - i];
+
+    }
+    return result;
+}
+
+//Три варианта вывода чисел Фибоначчисо сравнением времени выполнения
+
+    Console.WriteLine($"\n---Вывод первых N чисел Фибоначчи---\n");
+    
+double FibonacciBad(int n) // первый метод - через рекурсию напрямую
+{
+    if ((n == 1) || (n == 2)) return 1;
     else return FibonacciBad(n - 1) + FibonacciBad(n - 2);
 }
 
-double FibonacciR(double[] f, int n)
+double FibonacciR(double[] f, int n) // второй метод - через рекурсию с проверками
 {
     if (n == 1 || n == 2)
     {
@@ -111,6 +184,7 @@ double FibonacciR(double[] f, int n)
             return f[n - 1] = FibonacciR(f, n - 1) + FibonacciR(f, n - 2);
     }
 }
+    
 double FibonacciGood(int n)
 {
 
@@ -121,108 +195,44 @@ double FibonacciGood(int n)
 
 int countTests = 45;
 
-
 DateTime saveTime;
 
-Console.WriteLine($"Цикл for: ");
+Console.WriteLine($"Через цикл:\n"); // первый метод
+    
 saveTime = DateTime.Now;
 int fibonacci1 = 1;
 int fibonacci2 = 1;
-for (int i = 1; i <= countTests; i++)
+Console.WriteLine($"F({1}) = {fibonacci2}");   
+for (int i = 1; i < countTests; i++)
 {
-    Console.WriteLine($"f({i}) = {fibonacci2}");
+    Console.WriteLine($"f({i + 1}) = {fibonacci2}");
     int temporary = fibonacci2;
     fibonacci2 += fibonacci1;
     fibonacci1 = temporary;
 }
-Console.WriteLine($"Для for прошло: {DateTime.Now - saveTime}");
 
+    
+Console.WriteLine($"\nВремя нахождения первых {countTests} чисел Фибоначчи через цикл: {DateTime.Now - saveTime}");
 
-Console.WriteLine($"FibonacciBad: ");
+Console.WriteLine($"\nЧерез рекурсию с проверками:\n"); // второй метод
 saveTime = DateTime.Now;
+for (int i = 1; i <= countTests; i++)
+{
+    Console.WriteLine($"F({i}) = {FibonacciGood(i)}");
+}
+
+    
+Console.WriteLine($"\nВремя нахождения первых {countTests} чисел Фибоначчи через рекурсию с проверками: : {DateTime.Now - saveTime}");
+    
+    Console.WriteLine($"\nЧерез рекурсию напрямую:\n"); // третий метод
+    
+saveTime = DateTime.Now;
+    
 for (int i = 1; i <= countTests; i++)
 {
     Console.WriteLine($"F({i}) = {FibonacciBad(i)}");
 }
-Console.WriteLine($"Для FibonacciBad прошло: {DateTime.Now - saveTime}");
 
-
-Console.WriteLine($"FibonacciGood: ");
-saveTime = DateTime.Now;
-for (int i = 1; i <= countTests; i++)
-{
-
-    Console.WriteLine($"F({i}) = {FibonacciGood(i)}");
-}
-Console.WriteLine($"Для FibonacciR прошло: {DateTime.Now - saveTime}");
-System.Console.WriteLine();
-Console.ReadLine();
-
-//Напишите программу, которая перевернёт одномерный массив 
-//(последний элемент будет на первом месте, а первый - на последнем и т.д.)
-
-int[] array = GetArray();
-
-System.Console.WriteLine();
-
-Console.WriteLine("Array " + String.Join(" ", array));
-ReversArray1(array);
-Console.WriteLine("ReversArray1 : " + String.Join(" ", array));
-
-System.Console.WriteLine();
-
-Console.WriteLine("Array " + String.Join(" ", array));
-int[] reversArray = ReversArray2(array);
-Console.WriteLine("ReversArray2 : " + String.Join(" ", reversArray));
-
-System.Console.WriteLine();
-
-Console.WriteLine("Array " + String.Join(" ", array));
-ReversArray3(array);
-Console.WriteLine("ReversArray3 : " + String.Join(" ", array));
-
-int[] GetArray()
-{
-    Console.WriteLine("Введите размер массива: ");
-    int size = Convert.ToInt32(Console.ReadLine());
-
-    Console.WriteLine("Введите минимальное число массива: ");
-    int minValue = Convert.ToInt32(Console.ReadLine());
-
-    Console.WriteLine("Введите максимальное число массива: ");
-    int maxValue = Convert.ToInt32(Console.ReadLine());
-
-    int[] res = new int[size];
-
-    for (int i = 0; i < size; i++)
-    {
-        res[i] = new Random().Next(minValue, maxValue + 1);
-    }
-    return res;
-}
-
-void ReversArray1(int[] inArray)
-{
-    for (int i = 0; i < inArray.Length / 2; i++)
-    {
-        int numTemp = inArray[i];
-        inArray[i] = inArray[inArray.Length - i - 1];
-        inArray[inArray.Length - i - 1] = numTemp;
-    }
-}
-
-int[] ReversArray2(int[] inArray)
-{
-    int[] result = new int[inArray.Length];
-    for (int i = 0; i < inArray.Length; i++)
-    {
-        result[i] = inArray[inArray.Length - 1 - i];
-
-    }
-    return result;
-}
-
-void ReversArray3(int[] inArray)
-{
-    Array.Reverse(inArray);
+    Console.WriteLine($"\nВремя нахождения первых {countTests} чисел Фибоначчи через рекурсию напрямую: {DateTime.Now - saveTime}");
+  }
 }

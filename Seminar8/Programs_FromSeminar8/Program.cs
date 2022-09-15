12c6aa3e43472c5898850aacbae527a3331b9ff0
Scherbakov Anatoly <axidarr@gmail.com>
2022-09-15 23:21:01 +0300

    Temp seminars 8,9, HW 8

--- Seminar8/Programs_FromSeminar8/Program.cs
+++ Seminar8/Programs_FromSeminar8/Program.cs
@@ -1,4 +1,6 @@
-﻿// Задача 53: Задайте двумерный массив. Напишите программу,
+﻿Console.Clear();
+
+// Задача 53: Задайте двумерный массив. Напишите программу,
 // которая поменяет местами первую и последнюю строку массива.
 
 Console.WriteLine($"___Задача 53: Программа, меняющая местами первую и последнюю строку массива___\n");
@@ -8,8 +10,11 @@ System.Console.WriteLine($"Ваш массив №1:\n");
 PrintArray(arr1);
 
 int[,] arrTransposition = TranspositionArray(arr1);
-System.Console.WriteLine($"Массив №1, где заменены первая и последняя строки:\n");
+System.Console.WriteLine($"\nМассив №1, где заменены первая и последняя строки:\n");
 PrintArray(arrTransposition);
+
+Console.WriteLine($"\nНажмите любую клавишу для продолжения...\n");
+Console.ReadKey();
 
 // Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы.
 // В случае, если это невозможно, программа должна вывести сообщение для пользователя.
@@ -27,6 +32,8 @@ if (turnArr != null)
     PrintArray(turnArr);
 }
 
+Console.WriteLine($"\nНажмите любую клавишу для продолжения...\n");
+Console.ReadKey();
 
 // Задача 57: Составить частотный словарь элементов двумерного массива.
 // Частотный словарь содержит информацию о том, сколько раз встречается
@@ -40,42 +47,66 @@ PrintArray(arr3);
 
 Console.WriteLine();
 
+Console.WriteLine($"\nРешение с семинара:\n");
 MeetsCount(arr3);
-Console.WriteLine($"\nМой метод::\n");
+Console.WriteLine($"\nМоё решение:\n");
 MeetsCountMy(arr3);
+Console.WriteLine($"\nРешение комнаты 4:\n");
+SlovArr(arr3);
+Console.WriteLine($"\nРешение Наташи:\n");
+Dictionary();
 
-Console.WriteLine();
+Console.WriteLine($"\nНажмите любую клавишу для продолжения...\n");
+Console.ReadKey();
 
 // Задача 59: Задайте двумерный массив из целых чисел.
 // Напишите программу, которая удалит строку и столбец, на
-// пересечении которых расположен наименьший элемент
-// массива.
+// пересечении которых расположен наименьший элемент массива.
 // Например, задан массив:
 // 1 4 7 2
 // 5 9 2 3
 // 8 4 2 4
 // 5 2 6 7
-// Наименьший элемент - 1, на выходе получим
-// следующий массив:
+// Наименьший элемент - 1, на выходе получим следующий массив:
 // 9 4 2
 // 2 2 6
 // 3 4 7
 
-// Console.WriteLine($"___Задача 59: ___\n");
+Console.WriteLine($"___Задача 59: удаление строки и столбца на пересечении с минимальным элементом массива___\n");
 
-// int[,] arr4 = GetArrayInt();
-// Console.WriteLine($"Ваш массив №4:\n");
-// PrintArray(arr4);
+int[,] arr4 = GetArrayInt();
+Console.WriteLine($"Ваш массив №4:\n");
+PrintArray(arr4);
 
-// Console.WriteLine();
+//FindMinAndRemoveInArray(arr4);
 
+Console.WriteLine($"\nТут должно быть решение по удалению строки и столбца массива\n");
+Console.WriteLine($"\nНажмите любую клавишу для продолжения...\n");
+Console.ReadKey();
 
-// Задача 61: Вывести первые N строк треугольника
-// Паскаля. Сделать вывод в виде равнобедренного
-// треугольника
+// Задача 61: Вывести первые N строк треугольника Паскаля.
+// Сделать вывод в виде равнобедренного треугольника.
 
+// Решение Камянецкого
 
-int[,] GetArrayInt()
+Console.Clear();
+
+int row = 30;
+int[,] triangle = new int[row, row];
+const int cellWidth = 3;
+
+Console.ReadLine();
+FillTriangle();
+PrintTriangle();
+Console.WriteLine();
+Magic();
+
+Console.WriteLine($"\nНажмите любую клавишу для выхода...\n");
+Console.ReadKey();
+
+//----------------М-Е-Т-О-Д-Ы----------------   
+
+int[,] GetArrayInt() // 53, 55 ,57, 59
 {
     var rand = new Random();
 
@@ -85,14 +116,14 @@ int[,] GetArrayInt()
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = new Random().Next(10, 100);
+            array[i, j] = new Random().Next(1, 10);
         }
     }
 
     return array;
 }
 
-void PrintArray(int[,] array)
+void PrintArray(int[,] array) // 53, 55 ,57, 59
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
@@ -105,7 +136,7 @@ void PrintArray(int[,] array)
     }
 }
 
-int[,] TranspositionArray(int[,] array)
+int[,] TranspositionArray(int[,] array) // 53
 {
     for (int i = 0; i < array.GetLength(1); i++)
     {
@@ -119,7 +150,7 @@ int[,] TranspositionArray(int[,] array)
     return array;
 }
 
-int[,] TurnArrayAndPrint(int[,] array)
+int[,] TurnArrayAndPrint(int[,] array) // 55
 {
     int length = array.GetLength(0);
 
@@ -143,7 +174,7 @@ int[,] TurnArrayAndPrint(int[,] array)
     }
 }
 
-void MeetsCount(int[,] array)
+void MeetsCount(int[,] array) // 57
 {
     int[] mass = new int[100];
     for (int i = 0; i < array.GetLength(0); i++)
@@ -160,7 +191,7 @@ void MeetsCount(int[,] array)
     }
 }
 
-void MeetsCountMy(int[,] array)
+void MeetsCountMy(int[,] array) // 57 (моё решение)
 {
     int count = 0;
 
@@ -186,249 +217,187 @@ void MeetsCountMy(int[,] array)
     }
 }
 
-// int[] RemoveInArray(int[,] array) // 
-// {
-//     int min = array[0,0];
-
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             id
-//         }   
-//     }
-// }
-
-// int[,] GetArray()
-// {
-
-//     int[,] array = new int[new Random().Next(3, 6), new Random().Next(3, 6)];
-
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-
-//         {
-//             array[i, j] = new Random().Next(10, 100);
-//             Console.Write(array[i, j] + " ");
-//         }
-//         System.Console.WriteLine();
-//     }
-//     System.Console.WriteLine();
-//     System.Console.WriteLine();
-//     return array;
-// }
-
-//  int [,] TPArray(int [,] Array)
-//  {
-//      for (int i = 0; i < Array.GetLength(1); i++)
-//     {
-//         var temp = Array [Array.GetLength(0)-1,i];
-
-//         Array [Array.GetLength(0)-1,i] = Array [0,i];
-//         Array [0,i] = temp;    
-//     }   
-//     return Array;
-//   }    
-//   void PrintArray(int [,] array)
-//   {
-//      for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-
-//         {
-//             Console.Write(array[i, j] + " ");
-//         }
-//         Console.WriteLine();
-//     }
-//   }
-// PrintArray(TPArray(GetArray()));
-
-
-// зал по-иоему 4 
-// public static int[,] SlovArr(int [,] MyArr)
-//     {
-//         int rows = MyArr.GetLength(0), cols = MyArr.GetLength(1);
-//         int[,] slovar = new int[2, rows*cols];
-//         int count = 0;   //количество разных цифр в массиве
-//         slovar[0,0] =MyArr[0,0];
-//         for ( int i = 0; i < rows; i++ )
-//         {
-//             for (int j = 0; j < cols; j++)
-//             {
-//                 for (int k = 0; k < rows*cols; k++)
-//                 {
-//                     if  (slovar[0,k] == MyArr[i,j])
-//                     {
-//                         slovar [1, k] ++;
-//                         break;
-//                     } 
-//                     else 
-//                     {
-//                         count++;
-//                         slovar[0, count] = MyArr[i,j];
-//                         slovar[1, count] = 1;
-//                     }
-//                 }
-//             }
-//         }
-
-//         return slovar;
-//     }
-
-
-/* Задача 53: Задайте двумерный массив. Напишите программу,
-которая поменяет местами первую и последнюю строку
-массива. */
-// ____________________________________________________________________
-// int[,] CreateRandomTwoDimensionalArray()
-// {
-//     Random random = new Random();
-//     int rows = random.Next(3, 5);
-//     int columns = random.Next(3, 5);
-//     int[,] array = new int[rows, columns];
-
-//     for (int i = 0; i < rows; i++)
-//     {
-//         for (int j = 0; j < columns; j++)
-//         {
-//             array[i, j] = random.Next(0, 10);
-//             Console.Write($"{array[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-//     Console.WriteLine();
-//     return array;
-// }
-
-// int[,] SwapArray(int[,] arr)
-// {
-//     for (int i = 0; i < arr.GetLength(0); i++)
-//     {
-//             var temp = arr[arr.GetLength(0) - 1, i];
-//             arr [arr.GetLength(0) - 1, i] = arr[0, i];
-//             arr[0, i] = temp;
-//         }
-//     return arr;
-// }
-
-// void PrintArray(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             Console.Write($"{array[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-//     Console.WriteLine();
-// }
-
-// int[,] array = SwapArray(CreateRandomTwoDimensionalArray());
-// PrintArray(array);
-
-
-// /* Задача 55: Задайте двумерный массив. Напишите программу,
-// которая заменяет строки на столбцы. В случае, если это
-// невозможно, программа должна вывести сообщение для
-// пользователя. */
-// int[,] CreateRandomTwoDimensionalArray()
-// {
-//     Random random = new Random();
-//     int rows = random.Next(5, 6);
-//     int columns = random.Next(5, 6);
-//     int[,] array = new int[rows, columns];
-
-//     for (int i = 0; i < rows; i++)
-//     {
-//         for (int j = 0; j < columns; j++)
-//         {
-//             array[i, j] = random.Next(0, 10);
-//             Console.Write($"{array[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-//     Console.WriteLine();
-//     return array;
-// }
-
-// int[,] SwapArray(int[,] arr)
-// {
-//     int row = arr.GetLength(0);
-//     int columns = arr.GetLength(1);
-//     int[,] arrNew = new int[row, columns];
-//     if (row != columns)
-//     {
-//         Console.WriteLine("Ошибка");
-//         return new int[1,1];
-//     }
-//     else
-//     {
-//         for (int i = 0; i < row; i++)
-//         {
-//             for (int j = 0; j < columns; j++)
-//             {
-//                 arrNew[j, i] = arr[i, j];
-//             }
-//         }
-//         return arrNew;
-//     }
-// }
-// void PrintArray(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             Console.Write($"{array[i, j]} ");
-//         }
-//         Console.WriteLine();
-//     }
-//     Console.WriteLine();
-// }
-
-// int[,] array = SwapArray(CreateRandomTwoDimensionalArray());
-// PrintArray(array);
-
 // Метод Наташи
 
-// void Dictionary()
-// {
-//     System.Console.WriteLine("row:");
-//     int row = Convert.ToInt32(Console.ReadLine()!);
-//     System.Console.WriteLine("columns:");
-//     int columns = Convert.ToInt32(Console.ReadLine()!);
+void Dictionary() // 57 (решение Наташи)
+{
+    System.Console.WriteLine("row:");
+    int row = Convert.ToInt32(Console.ReadLine()!);
+    System.Console.WriteLine("columns:");
+    int columns = Convert.ToInt32(Console.ReadLine()!);
 
-//     System.Console.WriteLine("min");
-//     int min = Convert.ToInt32(Console.ReadLine()!);
-//     System.Console.WriteLine("max");
-//     int max = Convert.ToInt32(Console.ReadLine()!);
-//     System.Console.WriteLine();
-//     int[,] array = new int[row, columns];
-//    Console.WriteLine($"\n массив со случайными элементами от {min} до {max - 1}:\n");
-//     for (int i = 0; i < row; i++)
-//     {
-//         for (int j = 0; j < columns; j++)
-//         {
-//             array[i,j]=new Random().Next(min, max+1);
-//             System.Console.Write($"{array[i,j]}   ");
-//         }
-//         System.Console.WriteLine();
-//     }
+    System.Console.WriteLine("min");
+    int min = Convert.ToInt32(Console.ReadLine()!);
+    System.Console.WriteLine("max");
+    int max = Convert.ToInt32(Console.ReadLine()!);
+    System.Console.WriteLine();
+    int[,] array = new int[row, columns];
+    Console.WriteLine($"\n массив со случайными элементами от {min} до {max - 1}:\n");
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(min, max + 1);
+            System.Console.Write($"{array[i, j]}   ");
+        }
+        System.Console.WriteLine();
+    }
 
-//     int[]dictionary = new int[max+1];
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); j++)
-//         {
-//             dictionary[array[i,j]]+=1;
-//         }
-//     }
+    int[] dictionary = new int[max + 1];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            dictionary[array[i, j]] += 1;
+        }
+    }
 
-//     for (int i = 0; i < dictionary.Length; i++)
-//     {if (dictionary[i]>0)
-//         System.Console.WriteLine($"{i} встречается {dictionary[i]} раз(а)");
-//     }
-// }
-// Dictionary();
+    for (int i = 0; i < dictionary.Length; i++)
+    {
+        if (dictionary[i] > 0)
+            System.Console.WriteLine($"{i} встречается {dictionary[i]} раз(а)");
+    }
+}
+
+int[,] SlovArr(int[,] MyArr) // 57 (зал 4)
+{
+    int rows = MyArr.GetLength(0), cols = MyArr.GetLength(1);
+    int[,] slovar = new int[2, rows * cols];
+    int count = 0;   //количество разных цифр в массиве
+    slovar[0, 0] = MyArr[0, 0];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            for (int k = 0; k < rows * cols; k++)
+            {
+                if (slovar[0, k] == MyArr[i, j])
+                {
+                    slovar[1, k]++;
+                    break;
+                }
+                else
+                {
+                    count++;
+                    slovar[0, count] = MyArr[i, j];
+                    slovar[1, count] = 1;
+                    count = 0;
+                }
+            }
+        }
+    }
+
+    return slovar;
+}
+
+int FindMinInArray(int[,] array) // 59
+{
+    int min = array[0, 0];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] <= min)
+            {
+                min = array[i, j];
+            }
+        }
+    }
+
+    Console.WriteLine($"\nМинимальный элемент массива равен {min}");
+
+    return min;
+}
+
+int EntryOfUnitInArray(int[,] array, int num) // 59
+{
+    int entry = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == num)
+            {
+                entry++;
+            }
+        }
+    }
+
+    Console.WriteLine($"Этот элемент встречается {entry} раз\n");
+
+    return entry;
+}
+
+int[,] DeleteRowsColumnsInArray(int[,] array, int number, int entry = 1) // 59
+{
+    int[,] arrReduced = new int[array.GetLength(0) - entry, array.GetLength(1) - entry];
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] == number)
+            {
+
+            }
+        }
+    }
+    return arrReduced;
+}
+
+void FillTriangle() // 61
+{
+    for (int i = 0; i < row; i++)
+    {
+        triangle[i, 0] = 1;
+        triangle[i, i] = 1;
+    }
+
+    for (int i = 2; i < row; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            triangle[i, j] = triangle[i - 1, j - 1] + triangle[i - 1, j];
+        }
+    }
+}
+
+void PrintTriangle() // 61
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < row; j++)
+        {
+            if (triangle[i, j] != 0)
+                Console.WriteLine($"{triangle[i, j],cellWidth}");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Magic() // 61
+{
+    int col = cellWidth * row;
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            Console.SetCursorPosition(col, i + 1);
+            if (triangle[i, j] != 0) Console.Write($"{triangle[i, j],cellWidth}");
+            // if (triangle[i, j] % 2 != 0) Console.WriteLine("*");
+            col += cellWidth * 2;
+        }
+
+        col = cellWidth * row - cellWidth * (i + 1);
+
+        Console.WriteLine();
+    }
+}
+
+
+
+
+
